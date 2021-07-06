d64bbee4aec6289b873a7d74597f8068c748bfd8
IshaanSrivastavaaa <48021627+IshaanSrivastavaaa@users.noreply.github.com>
2021-07-06 22:30:01 +0530

    Update erc721-tokens.sol

--- contracts/token/erc721-tokens.sol
+++ contracts/token/erc721-tokens.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.7.6;
 
     import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
     import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
